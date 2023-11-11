7e180c77dbe9ad24c4b7da70fee09dfddb6f537b
Prashant <prashantyadav09783@gmail.com>
2023-11-11 18:49:08 +0530

    added style in profile page hackathon page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -18,7 +18,6 @@ const getCurrentUser = async (email: any) => {
   }
 };
 
-
 const Page = () => {
   const { data: session }: any = useSession();
 
@@ -55,32 +54,68 @@ const Page = () => {
     }
   }, [session?.user?.email]);
   return (
-    <div className="flex justify-evenly align-middle p-10">
-      <div>
-          <img
-            src={currentUser?.["avatar"]} 
-            alt="Profile Picture"
-            className="rounded-full h-30 w-30"
-          />
-          <p className="ml-10 mt-6 text-3xl">{currentUser?.["name"]}</p>
-          </div>
-          <div>
-            
-            <p className="m-4">Email: {currentUser?.["email"]}</p>
-            <p className="m-4">Username: {currentUser?.["username"]}</p>
-            <p className="m-4">Link: {currentUser?.["link"]}</p>
-            <p className="m-4">Repo: {currentUser?.["repo"]}</p>
-            
-            <p className="m-4">Hackathons:</p>
-            <ul>
-              {currentUser?.hackathon?.map((h:any,id:any)=>{
-                return(<li>{h}</li>)
-              })}
+
+  
+
+    //            
+    //             
+    //           </div>
+
+    //       </div>
+    //   );
+    // };
+    <>
+    <div className="bg-black">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8 sm:p-10 lg:flex-auto">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-600">
+            Email: {currentUser?.["email"]}
+            </h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Username: {currentUser?.["username"]}
+            </p>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+            Link: {currentUser?.["link"]}
+            </p>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+            Repo: {currentUser?.["repo"]}
+            </p>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+            Hackathons:  
+            </p>
+            <ul
+              role="list"
+              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+            {currentUser?.["hackathon"]?.map((h:any,id:any)=>{
+                    return(<li>{h}</li>)
+                  })}
             </ul>
+            <div className="mt-10 flex items-center gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                tags
+              </h4>
+            </div>
           </div>
-        
+          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="rounded-2xl bg-gray-900  text-center lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="mx-auto max-w-xs px-8">
+                <img
+                  src={currentUser?.["avatar"]}
+                  alt="Profile Picture"
+                  className="rounded-full h-35 w-35"
+                />
+                <p className="font-semibold text-gray-600 pt-10 text-1xl">
+                {currentUser?.["name"]}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+    </>
   );
 };
-
 export default Page;
--- src/components/ApplyHackathon.tsx
+++ src/components/ApplyHackathon.tsx
@@ -27,7 +27,7 @@ const ApplyHackathon = ({ id, userEmail }: any) => {
       onClick={() => {
         addUsertoHackathon();
       }}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
     >
       Apply
     </button>
